e2e86c73b6a2267f914f648a39e0b7131b5f8ca6
Catarina da Rosária <131613536+catarinadarosaria@users.noreply.github.com>
2023-07-10 14:55:14 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -32,7 +32,7 @@ function displayFahrenheitTemperature(event) {
 
   celsiusLink.classList.remove("active");
   fahrenheitLink.classList.add("active");
-  let fahrenheiTemperature = (celsiusTemperature * 9) / 5 + 32;
+  let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
   temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
 }
 
