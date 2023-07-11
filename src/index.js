fd1276bc355fb5292a4f8819f172decd6cacb72c
Catarina da Rosária <131613536+catarinadarosaria@users.noreply.github.com>
2023-07-11 17:28:27 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -36,7 +36,7 @@ function formatDay(timestamp) {
 //API call for Forecast
 function getForecast(coordinates) {
   let apiKey = "eff2995d6685ce8bb4f60a2ab4894754";
-  let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&unit=metric`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&unit=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
 
