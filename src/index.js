9b3f0ca96a91067615b40a97b242bc06a965cc76
Catarina da Rosária <131613536+catarinadarosaria@users.noreply.github.com>
2023-07-11 20:08:01 +0200

    costume icons

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-//Function to display Date and Time
+// Function to display Date and Time
 function formatDate(timestamp) {
   let date = new Date(timestamp);
   let hours = date.getHours();
@@ -31,14 +31,14 @@ function formatDay(timestamp) {
   return days[day];
 }
 
-//API call for Forecast
+// API call for Forecast
 function getForecast(coordinates) {
   let apiKey = "f3009e4852fa0a079dab291dabf020c4";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
 
-//Retreive General info from API
+// Retrieve General info from API
 function showTemperature(response) {
   let timestamp = response.data.dt * 1000;
   let dateTime = formatDate(timestamp);
@@ -60,12 +60,11 @@ function showTemperature(response) {
   let wind = document.querySelector("#wind");
   wind.innerHTML = `Wind: ${Math.round(response.data.wind.speed * 3.6)}km/h`;
 
+  let weatherCode = response.data.weather[0].icon;
   let iconElement = document.querySelector("#icon");
-  iconElement.setAttribute(
-    "src",
-    `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
-  );
-  iconElement.setAttribute("alt", response.data.weather[0].description);
+
+  iconElement.setAttribute("src", getIconPath(weatherCode));
+
   celsiusTemperature = response.data.main.temp;
 
   getForecast(response.data.coord);
@@ -107,7 +106,7 @@ function retrieveTemperature(city) {
   axios.get(apiUrl).then(showTemperature);
 }
 
-//API call Geolocalisation
+// API call Geolocation
 function retrieveCurrentLocationTemperature(position) {
   let latitude = position.coords.latitude;
   let longitude = position.coords.longitude;
@@ -121,7 +120,7 @@ function getCurrentLocationTemperature() {
   navigator.geolocation.getCurrentPosition(retrieveCurrentLocationTemperature);
 }
 
-//Search Engine for City Temperature
+// Search Engine for City Temperature
 let searchTemperature = document.querySelector("form");
 searchTemperature.addEventListener("submit", function (event) {
   event.preventDefault();
@@ -130,45 +129,93 @@ searchTemperature.addEventListener("submit", function (event) {
   retrieveTemperature(city);
 });
 
-//Search Engine for Geolocation City
+// Search Engine for Geolocation City
 let currentLocationButton = document.querySelector("#current-location-button");
 currentLocationButton.addEventListener("click", getCurrentLocationTemperature);
 
-//Forecast Construction
+// Forecast Construction
 function displayForecast(response) {
   let forecast = response.data.daily;
-
   let forecastElement = document.querySelector("#forecast");
 
-  let forecastHTML = `<div class="row">`;
+  let forecastHTML = '<div class="row">';
   forecast.forEach(function (forecastDay, index) {
     if (index < 6) {
-      forecastHTML =
-        forecastHTML +
-        `
-      <div class="col-2 forecast-column">
-        <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
-        <img
-          src="http://openweathermap.org/img/wn/${
-            forecastDay.weather[0].icon
-          }@2x.png"
-          alt=""
-          width="42"
-        />
-        <div class="weather-forecast-temperatures">
-          <span class="weather-forecast-temperature-max">${Math.round(
-            forecastDay.temp.max
-          )}º</span>
-          <span class="weather-forecast-temperature-min"> ${Math.round(
-            forecastDay.temp.min
-          )}º </span>
+      let iconCode = forecastDay.weather[0].icon;
+      let iconPath = getIconPath(iconCode);
+
+      forecastHTML += `
+        <div class="col-2 forecast-column">
+          <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+          <img src="${iconPath}" alt="" width="42" />
+          <div class="weather-forecast-temperatures">
+            <span class="weather-forecast-temperature-max">${Math.round(
+              forecastDay.temp.max
+            )}º</span>
+            <span class="weather-forecast-temperature-min">${Math.round(
+              forecastDay.temp.min
+            )}º</span>
+          </div>
         </div>
-      </div>
-  `;
+      `;
     }
   });
 
-  forecastHTML = forecastHTML + `</div>`;
+  forecastHTML += "</div>";
   forecastElement.innerHTML = forecastHTML;
 }
+
+// Get the path for the custom weather icon
+function getIconPath(weatherCode) {
+  let prefix = "images/";
+  let suffix = ".png";
+
+  if (weatherCode.endsWith("d")) {
+    // Daytime weather
+    switch (weatherCode) {
+      case "01d":
+        return prefix + "sun.png";
+      case "02d":
+        return prefix + "sun-cloud.png";
+      case "03d":
+        return prefix + "cloud.png";
+      case "04d":
+        return prefix + "clouds.png";
+      case "09d":
+        return prefix + "rain.png";
+      case "10d":
+        return prefix + "sun_rain.png";
+      case "11d":
+        return prefix + "storm.png";
+      case "13d":
+        return prefix + "snow.png";
+      case "50d":
+        return prefix + "windy.png";
+    }
+  } else if (weatherCode.endsWith("n")) {
+    // Nighttime weather
+    switch (weatherCode) {
+      case "01n":
+        return prefix + "night.png";
+      case "02n":
+        return prefix + "night_cloud.png";
+      case "03n":
+        return prefix + "cloud.png";
+      case "04n":
+        return prefix + "clouds.png";
+      case "09n":
+        return prefix + "rain.png";
+      case "10n":
+        return prefix + "night_rain.png";
+      case "11n":
+        return prefix + "night_thunder.png";
+      case "13n":
+        return prefix + "snow.png";
+      case "50n":
+        return prefix + "windy.png";
+    }
+  }
+}
+
+// Set initial city temperature
 retrieveTemperature("Porto");
